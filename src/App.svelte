f699f386db77e8191c9f3f760521598cd4ff2d6e
Ivan Kashirin <kashirinivan@icloud.com>
2025-08-02 17:00:41 +0200

    fix: treasurer's last name

--- src/App.svelte
+++ src/App.svelte
@@ -372,7 +372,7 @@
             >
           </p>
           <p>
-            <span class="black">Olga Slivko — </span><span class="blue"
+            <span class="black">Olga Wester — </span><span class="blue"
               >penningmeester</span
             >
           </p>
